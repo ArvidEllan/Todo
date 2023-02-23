307b5682239c11bdbb4b7ec7a5a7a02b15545ea5
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-23 10:39:14 +0300

    update func services

--- service.go
+++ service.go
@@ -25,4 +25,29 @@ func (s *TodosApiService) TodosPost(ctx *core.MifyRequestContext, todoNoteCreate
 	//return openapi.Response(500, Error{}), nil
 
 	return openapi.Response(http.StatusNotImplemented, nil), errors.New("TodosPost method not implemented")
+
+
+// TodosPost - Add new todo note
+func (s *TodosApiService) TodosPost(
+	ctx *core.MifyRequestContext, todoNoteCreateRequest openapi.TodoNoteCreateRequest) (openapi.ServiceResponse, error) {
+	todoSvc := apputil.GetServiceExtra(ctx.ServiceContext()).TodoService
+
+	note, err := todoSvc.CreateTodo(ctx, domain.TodoNote{
+		Title: todoNoteCreateRequest.Title,
+		Description: todoNoteCreateRequest.Description,
+	})
+	if err != nil {
+		// Pass some user friendly error to user instead of internal one,
+		// which will be available in service logs. Since we don't expect
+		// any errors here, we return Internal Server Error, which is catch-all
+		// for all unknown bad things. These kind of errors will be reported in
+		// metrics and it's easy to set alerts for them.
+		return openapi.Response(http.StatusInternalServerError, openapi.Error{
+			Code: strconv.Itoa(http.StatusInternalServerError),
+			Message: "Failed to create todo note",
+		}), err
+	}
+
+	return openapi.Response(http.StatusOK, handlers.MakeAPITodoNote(note)), nil
+}
 }
